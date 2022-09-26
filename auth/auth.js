0ab76b0f2d06e4c6ce6d9100e7bbb320ccb1d634
Sam Adams <sam.adams7785@gmail.com>
2022-09-26 14:17:25 -0700

    part A

--- auth/auth.js
+++ auth/auth.js
@@ -71,8 +71,10 @@ authForm.addEventListener('submit', async (e) => {
 
     // > Part A:
     //      - get formData object from form
+    const formData = new FormData(authForm);
     //      - call "authType.action" passing in the email and password from
     //        the form data and assign to response variable
+    const response = await authType.action(formData.get('email'), formData.get('password'));
 
     const error = response.error;
 
@@ -84,5 +86,6 @@ authForm.addEventListener('submit', async (e) => {
     } else {
         // go back to wherever user came from
         // > Part A using "location", replace url with "redirectUrl"
+        location.replace(redirectUrl);
     }
 });
